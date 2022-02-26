ad94257af37a2ab302c19012dffa95e4bf6570d2
LeonDan <leonov.denlew@yandex.ru>
2022-02-26 18:25:07 +0300

    add logic

--- src/dleon/Tea.java
+++ src/dleon/Tea.java
@@ -3,11 +3,11 @@ package dleon;
 public class Tea extends Beverage {
     @Override
     public void brew() {
-
+        System.out.println("Steeping the tea");
     }
 
     @Override
     public void addCondiments() {
-
+        System.out.println("Adding lemon");
     }
 }
